fdf633fe0488558c62685a5331a746f3ee54e7c9
Karim Chehade <kim.chehade@gmail.com>
2023-10-31 10:04:07 +0200

    edit

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -1,34 +1,57 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from 'react-router-dom';
 import axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
     const [posts, setPosts] = useState([]);
-
-    const category = useLocation().search
+    const [searchQuery, setSearchQuery] = useState('');
+    
+    const category = useLocation().search;
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-              const res = await axios.get(`/posts${category}`); 
-              setPosts(res.data);
+                const res = await axios.get(`/posts${category}`);
+                setPosts(res.data);
             } catch (err) {
-              console.log("Axios Error:", err);
-              console.log("Response Data:", err.response.data);
+                console.log("Axios Error:", err);
+                console.log("Response Data:", err.response.data);
             }
-          };
+        };
         fetchData();
     }, [category]);
 
     const getText = (html) => {
         const doc = new DOMParser().parseFromString(html, "text/html");
         return doc.body.textContent;
-      }
-      
+    }
+
+    // Handle search input change
+    const handleSearchInputChange = (e) => {
+        setSearchQuery(e.target.value);
+    }
+
+    // Filter posts based on search query
+    const filteredPosts = posts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()));
+
     return (
         <div className="home">
+            <div className="search">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={handleSearchInputChange}
+                />
+                <button className="search-button">
+                    <FontAwesomeIcon icon={faSearch} />
+                </button>
+            </div>
             <div className="posts">
-                {posts.map(post => (
+                {filteredPosts.map(post => (
                     <div className="post" key={post.id}>
                         <div className="image">
                             <img src={`../upload/${post.image}`} alt="Pic here" />
@@ -41,7 +64,6 @@ const Home = () => {
                             <button>
                                 <Link className="btn" to={`/post/${post.id}`}>Read More</Link>
                             </button>
-
                         </div>
                     </div>
                 ))}
@@ -49,6 +71,5 @@ const Home = () => {
         </div>
     )
 }
-
 
 export default Home;
